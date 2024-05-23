882e6c5a38804886218c14ba90fcad97bf5694b5
VictorRayas <20213tn082@utez.edu.mx>
2024-05-22 20:17:29 -0600

    Mi first Commit

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 
 from hello_world import app
@@ -63,10 +61,5 @@ def apigw_event():
 
 
 def test_lambda_handler(apigw_event):
-
     ret = app.lambda_handler(apigw_event, "")
-    #data = json.loads(ret["body"])
-
     assert ret["statusCode"] == 200
-    #assert "message" in ret["body"]
-    #assert data["message"] == "hello world"
